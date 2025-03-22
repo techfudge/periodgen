28377d715fa10af51c1a6e1930ad808a200ba85a
techfudge <bobobusari11@gmail.com>
2025-03-22 17:57:44 +0100

    third

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker Installed");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
+  });
+  
